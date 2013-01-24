23ff74849bcd8a896a1156d1c6a8b41d6159c627
Ferrix Hovi <ferrix@ferrix.fi>
2013-01-24 16:40:21 +0200

    Old pending changes
    * Setting X11 keyboard
    * Beeping off

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ cp xmobarrc ~/.xmobarrc
 cp Xresources ~/.Xresources
 cp inputrc ~/.inputrc
 cp gitconfig ~/.gitconfig
+sudo cp etc/11-keyboard.conf /etc/X11/xorg.conf.d/11-keyboard.conf
 mkdir -p ~/.xmonad
 cp xmonad/xmonad.hs ~/.xmonad
 xmonad --recompile
